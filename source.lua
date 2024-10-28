a69096b98273494c2f315dbee490365b27586802
death <88369910+d38th@users.noreply.github.com>
2024-10-27 21:05:52 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,7 +1,7 @@
 local Luxt1 = {}
 
 function Luxt1.CreateWindow(libName, logoId)
-    local LuxtLib = Instance.new("ScreenGui")
+    local HugeLib = Instance.new("ScreenGui")
     local shadow = Instance.new("ImageLabel")
     local MainFrame = Instance.new("Frame")
     local sideHeading = Instance.new("Frame")
@@ -48,10 +48,10 @@ function Luxt1.CreateWindow(libName, logoId)
     game:GetService("UserInputService").InputBegan:connect(function(current, ok) 
         if not ok then 
             if current.KeyCode.Name == oldKey then 
-                if LuxtLib.Enabled == true then
-                    LuxtLib.Enabled = false
+                if HugeLib.Enabled == true then
+                    HugeLib.Enabled = false
                 else
-                    LuxtLib.Enabled = true
+                    HugeLib.Enabled = true
                 end
             end
         end
@@ -106,13 +106,13 @@ function Luxt1.CreateWindow(libName, logoId)
     pageFolder.Parent = framesAll
 
     --
-    libName = libName or "LuxtLib"
+    libName = libName or "HugeLib"
     logoId = logoId or ""
     --
 
-    LuxtLib.Name = "LuxtLib"..libName
-    LuxtLib.Parent = game.CoreGui
-    LuxtLib.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    HugeLib.Name = "HugeLib"..libName
+    HugeLib.Parent = game.CoreGui
+    HugeLib.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
     MainFrame.Name = "MainFrame"
     MainFrame.Parent = shadow
@@ -221,7 +221,7 @@ function Luxt1.CreateWindow(libName, logoId)
     framesAll.ZIndex = 2
 
     shadow.Name = "shadow"
-    shadow.Parent = LuxtLib
+    shadow.Parent = HugeLib
     shadow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     shadow.BackgroundTransparency = 1.000
     shadow.Position = UDim2.new(0.319562584, 0, 0.168689325, 0)
